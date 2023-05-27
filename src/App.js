ff4eb4e8d45124530672fd72be4045107030f22d
echizenyayota <virtualmisso30@gmail.com>
2023-05-27 22:02:34 +0900

    07:32 Getting data into our App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,27 @@
-const App = () => {
+import axios from "axios";
+
+const App = async () => {
+
+  const options = {
+    method: 'GET',
+    url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
+    params: {
+      level: '3',
+      area: 'sat'
+    },
+    headers: {
+      'X-RapidAPI-Key': 'fa98a7312emshb86681091093840p1d9006jsn39a816de511c',
+      'X-RapidAPI-Host': 'twinword-word-association-quiz.p.rapidapi.com'
+    }
+  };
+
+  try {
+    const response = await axios.request(options);
+    console.log(response.data);
+  } catch (error) {
+    console.error(error);
+  }
+
   return (
     <div className="App">
 
