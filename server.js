7741a7b518c0e5888c5ef32ed576107f184b4b1c
echizenyayota <virtualmisso30@gmail.com>
2023-06-02 20:21:10 +0900

    13:33 remove the rapid api key and the rapid api host in the frontend (Passing Parameters from Frontend to Backend)

--- server.js
+++ server.js
@@ -3,8 +3,9 @@ const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
 const axios = require("axios");
-
 const app = express();
+
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.json("hi!");
