103aa41625762f3e2a43fecb213140037d4af47a
echizenyayota <virtualmisso30@gmail.com>
2023-06-02 20:11:07 +0900

    11:54 move the code snippets of the API in the frontend to the backend (Passing Parameters from Frontend to Backend)

--- src/App.js
+++ src/App.js
@@ -10,26 +10,6 @@ const App = () => {
   const [score, setScore] = useState(0);
 
   const getRandomWords = async() => {
-    const options = {
-      method: 'GET',
-      url: 'https://twinword-word-association-quiz.p.rapidapi.com/type1/',
-      params: {
-        level: chosenLevel,
-        area: 'sat'
-      },
-      headers: {
-        'X-RapidAPI-Key': process.env.REACT_APP_RAPID_API_KEY,
-        'X-RapidAPI-Host': 'twinword-word-association-quiz.p.rapidapi.com'
-      }
-    };
-  
-    try {
-      const response = await axios.request(options);
-      console.log(response.data);
-      setWords(response.data);
-    } catch (error) {
-      console.error(error);
-    }
   }
   
   console.log(words && words.quizlist);
